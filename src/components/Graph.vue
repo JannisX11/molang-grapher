de8720db642d9ed1e64a39d65c1e377cece926f0
JannisX11 <jannis4236@gmail.com>
2025-07-14 00:30:59 +0200

    Update dependencies

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -27,6 +27,7 @@ const X_variables = [
 	'query.anim_time',
 	'query.life_time',
 	'time',
+	'xx',
 	'query.modified_distance_moved',
 	'variable.particle_age',
 	'variable.emitter_age'
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,20 +1,39 @@
 let mix = require('laravel-mix');
+let path = require('path');
+let fs = require('fs');
+const {DefinePlugin} = require('webpack')
+
+process.argv.push('--https')
+
 
 mix.webpackConfig({
     module: {
         rules: [
 			{
-				test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-				loader: require.resolve('url-loader'),
-				options: {
-				  limit: 100000,
-				  name: 'static/media/[name].[hash:8].[ext]',
-				},
-			  },
-        ],
-    }
+				test: /\.png/,
+				type: 'asset/inline',
+				generator: {
+					dataUrl: content => {
+						let path = './dist' + content.toString().split('"')[1].replace(/\?.*/, '');
+						let base64 = require('fs').readFileSync(path, {encoding: 'base64'});
+						return 'data:image/png;base64,' + base64;
+					}
+				}
+			}
+		],
+	},
+	plugins: [
+        new DefinePlugin({
+            VERSION: `"${require('./package.json').version}"`
+        })
+	],
+	devServer: {
+		contentBase: path.join(__dirname, 'dist'),
+		port: 3000,
+		open: true
+	}
 });
 
 mix.js('src/app.js', 'dist/')
-    .setPublicPath('dist');
-    
+	.vue()
+	.setPublicPath('dist');
